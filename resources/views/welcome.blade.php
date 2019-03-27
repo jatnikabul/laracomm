e1eb166c05b5aef7d36e7adb7a2aab4d3d110510
kabul jatnika abin <kabuljatnikaabin@gmail.com>
2019-03-28 01:34:53 +0700

    mengganti judul

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laracom
                 </div>
 
                 <div class="links">
